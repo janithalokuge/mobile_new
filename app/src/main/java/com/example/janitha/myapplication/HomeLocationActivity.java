9c7277f59ef39c2db80a9b6e9b347db1882c4be5
Menaka <menaka.12@cse.mrt.ac.lk>
2016-11-02 03:02:01 +0530

    added following functionality: -draw a circle to show the fence with the center being the point of the marker -displays current Lat and Long values of the home location -changes the radius of the circle with text being changed in the Fence radius edit text field

--- app/src/main/java/com/example/janitha/myapplication/HomeLocationActivity.java
+++ app/src/main/java/com/example/janitha/myapplication/HomeLocationActivity.java
@@ -7,19 +7,28 @@ import com.example.janitha.myapplication.MainActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.text.DecimalFormat;
 
 public class HomeLocationActivity extends AppCompatActivity implements OnMapReadyCallback{
 
@@ -27,6 +36,13 @@ public class HomeLocationActivity extends AppCompatActivity implements OnMapRead
     private GoogleMap map;
     private Location lastLocation;
     private Marker currentMarker;
+    private  Circle circle;
+
+    TextView textView_LatitudeValue;
+    TextView textView_LongitudeValue;
+    EditText editText_FenceRadius;
+    String fenceSettingsRadius = "500m";
+    private int fenceRadius;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +51,35 @@ public class HomeLocationActivity extends AppCompatActivity implements OnMapRead
 
         mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.fragment_home_map);
         mapFragment.getMapAsync(this);
+
+        textView_LatitudeValue = (TextView)findViewById(R.id.textView_LatitudeValue);
+        textView_LongitudeValue = (TextView)findViewById(R.id.textView_LongitudeValue);
+        editText_FenceRadius = (EditText)findViewById(R.id.editText_FenceRadius);
+        fenceRadius = Integer.parseInt(editText_FenceRadius.getText().toString());
+
+        editText_FenceRadius.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try {
+                    fenceRadius = Integer.parseInt(editText_FenceRadius.getText().toString());
+                    drawCircle(new LatLng(currentMarker.getPosition().latitude, currentMarker.getPosition().longitude), fenceRadius);
+                }
+                catch (NumberFormatException e) { // When converting a null string to int
+                    e.printStackTrace();
+                    drawCircle(new LatLng(currentMarker.getPosition().latitude, currentMarker.getPosition().longitude), 0);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
     }
 
@@ -64,10 +109,16 @@ public class HomeLocationActivity extends AppCompatActivity implements OnMapRead
                     .draggable(true));
         }
 
+        updateLatLng();
+
+        drawCircle(initialLatLng,fenceRadius);  //draw the (circular)boundary of the Fence
+
         map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
             public void onMapClick(LatLng latLng) {
                 drawMarker(latLng);
+                updateLatLng();
+                drawCircle(latLng,fenceRadius);
             }
         });
     }
@@ -78,5 +129,27 @@ public class HomeLocationActivity extends AppCompatActivity implements OnMapRead
                 .position(latLng)
                 .title("Current Position")
                 .draggable(true));
+    }
+
+    public void updateLatLng(){
+        if(currentMarker !=null){
+            textView_LatitudeValue.setText(new DecimalFormat("#.000").format(currentMarker.getPosition().latitude));
+            textView_LongitudeValue.setText(new DecimalFormat("#.000").format(currentMarker.getPosition().longitude));
+        }
+    }
+
+    public void drawCircle(LatLng latLng, int radius){
+        if(currentMarker != null){
+            if (circle != null) {
+                circle.remove();
+            }
+            circle = map.addCircle(new CircleOptions()
+                    .center(latLng)
+                    .radius(radius)
+                    .strokeColor(Color.RED)
+                    .fillColor(0x44ff0000)
+                    .strokeWidth(7));
+        }
+
     }
 }
