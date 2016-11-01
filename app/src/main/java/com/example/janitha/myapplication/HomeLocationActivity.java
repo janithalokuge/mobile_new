22382ccef7ac6f880a71135586b15700d762089b
Menaka <menaka.12@cse.mrt.ac.lk>
2016-11-01 23:50:47 +0530

    -changed the app name :) -draw the marker at location on the map that the user clicked

--- app/src/main/java/com/example/janitha/myapplication/HomeLocationActivity.java
+++ app/src/main/java/com/example/janitha/myapplication/HomeLocationActivity.java
@@ -7,20 +7,26 @@ import com.example.janitha.myapplication.MainActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class HomeLocationActivity extends AppCompatActivity implements OnMapReadyCallback{
 
-    public MapFragment mapFragment;
+    private MapFragment mapFragment;
+    private GoogleMap map;
+    private Location lastLocation;
+    private Marker currentMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +39,14 @@ public class HomeLocationActivity extends AppCompatActivity implements OnMapRead
     }
 
     @Override
-    public void onMapReady(GoogleMap map){
+    public void onMapReady(GoogleMap tempMap){
+        this.map=tempMap;
         map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+//        Location lastLocation = MainActivity.homeLastLocation;
+        lastLocation = getIntent().getExtras().getParcelable(MainActivity.LAST_LOCATION);
+        LatLng initialLatLng = new LatLng(lastLocation.getLatitude(),lastLocation.getLongitude());
+        map.moveCamera(CameraUpdateFactory.newLatLng(initialLatLng));
+
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
@@ -45,10 +57,26 @@ public class HomeLocationActivity extends AppCompatActivity implements OnMapRead
                     Toast.LENGTH_LONG).show();
         }
 
-        if (MainActivity.homeLastLocation != null) {
-            map.addMarker(new MarkerOptions()
-                    .position(new LatLng(MainActivity.homeLastLocation.getLatitude(),MainActivity.homeLastLocation.getLongitude()))
-                    .title("Current Position"));
+        if (lastLocation != null) {
+            currentMarker = map.addMarker(new MarkerOptions()
+                    .position(initialLatLng)
+                    .title("Current Position")
+                    .draggable(true));
         }
+
+        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                drawMarker(latLng);
+            }
+        });
+    }
+
+    public void drawMarker(LatLng latLng){
+        currentMarker.remove();
+        currentMarker = map.addMarker(new MarkerOptions()
+                .position(latLng)
+                .title("Current Position")
+                .draggable(true));
     }
 }
